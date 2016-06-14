2ec37f4d699173192cc00443d8f549b24fb80605
Hudo Assenco <hudo.assenco@gmail.com>
2016-06-14 14:38:35 -0700

    Update

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.use(bodyParser.json());
 
 //Site - Routes ==================================================
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.header("Access-Control-Allow-Origin", "http://localhost:8080");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 });
@@ -58,7 +58,7 @@ app.get('/', function(req, res, next) {
   return res.status(200).json({
     attr: "Qualquer merda",
     outraKey: "Outra merda",
-    array: ["Varias merdas", "1", "2"]
+    arr.ay: ["Varias merdas", "1", "2"]
   });
 });
 
