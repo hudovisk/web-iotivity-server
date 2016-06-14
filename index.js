7ab86bf56da9dbed498b1f7d162856bd91bdff2d
Hudo Assenco <hudo.assenco@gmail.com>
2016-06-14 14:41:21 -0700

    Update

--- index.js
+++ index.js
@@ -49,8 +49,10 @@ app.use(bodyParser.json());
 
 //Site - Routes ==================================================
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:8080");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.setHeader('Access-Control-Allow-Origin', "http://"+req.headers.host+':8080');
+
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization');
   next();
 });
 
